9785f278850057dbba7151cf90c5f4e75a4e3e5e
Christian Schriewer <c.schriewer@shopmacher.de>
2024-12-05 12:09:49 +0100

    Fix missing webhookId

--- paypal-commercetools-extension/src/connector/post-deploy.ts
+++ paypal-commercetools-extension/src/connector/post-deploy.ts
@@ -14,11 +14,17 @@ import {
   createPaymentUpdateExtension,
   deleteAccessTokenIfExists,
 } from './actions';
+import { PAYPAL_EXTENSION_PATH } from "../routes/service.route";
+import { PAYPAL_WEBHOOKS_PATH } from "../routes/webhook.route";
 
 const CONNECT_APPLICATION_URL_KEY = 'CONNECT_SERVICE_URL';
 
 async function postDeploy(properties: Map<string, unknown>): Promise<void> {
   const applicationUrl = properties.get(CONNECT_APPLICATION_URL_KEY);
+  const webhookUrl = process.env.CONNECT_SERVICE_URL?.replace(
+      PAYPAL_EXTENSION_PATH,
+      PAYPAL_WEBHOOKS_PATH
+  ) ?? '';
 
   assertString(applicationUrl, CONNECT_APPLICATION_URL_KEY);
 
@@ -29,8 +35,8 @@ async function postDeploy(properties: Map<string, unknown>): Promise<void> {
   await createCustomPaymentType(apiRoot);
   await createCustomCustomerType(apiRoot);
   await createCustomPaymentInteractionType(apiRoot);
-  await createWebhook();
-  await createAndSetCustomObject(apiRoot);
+  await createWebhook(webhookUrl);
+  await createAndSetCustomObject(apiRoot, webhookUrl);
 }
 
 async function run(): Promise<void> {
